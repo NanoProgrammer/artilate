3b8c168449b27407481b943869fde35ca86df749
NanoProgrammer <satiagogonzalezjaimes77@gmail.com>
2025-04-07 22:11:05 -0600

    SEO and favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,21 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://artilate.com',
   adapter: cloudflare(),
   output: 'server',
+
   vite: {
-    assetsInclude: ['src/assets/**/*.jpg'], // Incluye todos los archivos en src/assets
+    // Incluye todos los archivos en src/assets
+    assetsInclude: ['src/assets/**/*.jpg'],
+
+    plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
